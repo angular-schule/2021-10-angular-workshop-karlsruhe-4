5abe2ada03a3a8cd5cd1af35b3b884f3abb6e16e
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-01-12 13:36:12 +0100

    ts: constructor Shorthand

--- typescript-playground/main.ts
+++ typescript-playground/main.ts
@@ -1,1 +1,9 @@
-console.log('Hello ngWorld! 🤩')
+class Test {
+
+    constructor(public antwort: number) {
+        console.log('Hello ngWorld! 🤩', this.antwort = antwort)
+    }
+}
+
+const test = new Test(42);
+test.antwort
