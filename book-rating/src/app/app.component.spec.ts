f38af8d6d5b3632bdc8d3d452afa5135349a7832
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-01-17 11:16:27 +0100

    shallow unit tests

--- book-rating/src/app/app.component.spec.ts
+++ book-rating/src/app/app.component.spec.ts
@@ -1,6 +1,17 @@
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+
+/*
+@Component({
+  selector: 'br-dashboard',
+  template: ':-)'
+})
+export class DashboardDummyComponent {
+}
+*/
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,8 +20,10 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        // DashboardDummyComponent // echter, echter Unit Test
       ],
+      schemas: [NO_ERRORS_SCHEMA] // Shallow Unit Test
     }).compileComponents();
   });
 
