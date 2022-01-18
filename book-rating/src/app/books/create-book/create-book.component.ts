06a0e3637c451e6a4859a50361848f78cba783fb
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-01-18 13:19:58 +0100

    bookForm.markAllAsTouched()

--- book-rating/src/app/books/create-book/create-book.component.ts
+++ book-rating/src/app/books/create-book/create-book.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-create-book',
@@ -7,6 +8,9 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./create-book.component.scss']
 })
 export class CreateBookComponent  {
+
+  @Output()
+  create = new EventEmitter<Book>();
 
   bookForm = new FormGroup({
     isbn: new FormControl('', [Validators.required, Validators.minLength(3)]),
@@ -25,15 +29,13 @@ export class CreateBookComponent  {
       rating: 1
     }
 
-    /// HandsOn
-    /// 1. Erzeuge ein Event mit dem Namen 'create'
-    /// 2. Emittiere das Event
-    /// 3. Subscribe dich auf das Event im Dashboard
-    /// 4. Füge das Buch dem Array hinzu (Achtung: Immutability)
+    if (this.bookForm.invalid) {
+      this.bookForm.markAllAsTouched();
+      return;
+    }
 
-
+    this.create.emit(newBook);
     this.bookForm.reset();
-
   }
 
 }
