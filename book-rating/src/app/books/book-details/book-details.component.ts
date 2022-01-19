92ef0ff0193566e1e72fe6527c6a638b708b1fe3
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-01-19 14:15:03 +0100

    rjxs: subscribung once

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { concatMap, map, mergeMap } from 'rxjs';
+import { concatMap, map, mergeMap, switchMap } from 'rxjs';
 import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
@@ -12,7 +12,7 @@ export class BookDetailsComponent {
 
   book$ = this.route.paramMap.pipe(
     map(paramMap => paramMap.get('isbn')),
-    concatMap(isbn => this.bs.getSingleBook(isbn!))
+    switchMap(isbn => this.bs.getSingleBook(isbn!))
   );
 
   constructor(
