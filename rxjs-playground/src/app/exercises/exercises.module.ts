82fdb9f0c7478bfaa139ca2bb2a24bf4bd981e7c
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2022-01-19 10:57:28 +0100

    sorry!!

--- rxjs-playground/src/app/exercises/exercises.module.ts
+++ rxjs-playground/src/app/exercises/exercises.module.ts
@@ -41,7 +41,7 @@ const routes: Routes = [
     HistoryComponent,
     HigherorderComponent,
     GameScoreComponent,
-    ErrorHandlingComponent,
+    // ErrorHandlingComponent,
     ChatComponent,
     ChatWindowComponent,
     UnsubscribeComponent,
